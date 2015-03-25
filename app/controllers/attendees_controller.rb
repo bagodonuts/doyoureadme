3fe2d24db7a4a59f3512a705fbd8393c204edb7c
Jim Berrettini <james.berrettini@gmail.com>
2015-03-24 22:40:40 -0400

    Add ability to see attendee list for a seminar

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,4 +1,11 @@
 class AttendeesController < ApplicationController
   def index
+    @seminar = Seminar.find(attendee_params[:seminar_id])
+    @attendees = @seminar.users
+  end
+
+  private
+  def attendee_params
+    params.permit(:seminar_id)
   end
 end
